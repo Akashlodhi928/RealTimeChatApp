e58960c1058deab43d890a4ad69caa4098203ddc
Akashlodhi <127076693+Akashlodhi928@users.noreply.github.com>
2025-11-02 20:26:44 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://realtimechatapp-28vr.onrender.com",
     credentials:true
 }))
 
